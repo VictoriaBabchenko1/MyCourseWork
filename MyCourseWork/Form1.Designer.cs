c1f8596d23ecaf7847ddd840fa126214f4227ad2
Victoria <victoria.babchenko30@gmail.com>
2023-12-31 04:18:56 +0200

    Добавьте файлы проекта.

---
+++ MyCourseWork/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace MyCourseWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.startDateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(21, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(226, 384);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // startDateTimePicker1
+            // 
+            this.startDateTimePicker1.Location = new System.Drawing.Point(344, 216);
+            this.startDateTimePicker1.Name = "startDateTimePicker1";
+            this.startDateTimePicker1.Size = new System.Drawing.Size(189, 27);
+            this.startDateTimePicker1.TabIndex = 3;
+            this.startDateTimePicker1.ValueChanged += new System.EventHandler(this.startDateTimePicker1_ValueChanged);
+            // 
+            // endDateTimePicker2
+            // 
+            this.endDateTimePicker2.Location = new System.Drawing.Point(577, 216);
+            this.endDateTimePicker2.Name = "endDateTimePicker2";
+            this.endDateTimePicker2.Size = new System.Drawing.Size(194, 27);
+            this.endDateTimePicker2.TabIndex = 4;
+            this.endDateTimePicker2.ValueChanged += new System.EventHandler(this.endDateTimePicker2_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(383, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(424, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(453, 270);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(80, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(400, 345);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(539, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(253, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "label5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(253, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(523, 83);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(77, 27);
+            this.textBox5.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(253, 348);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "label8";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 51);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(453, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 20);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(442, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 20);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.endDateTimePicker2);
+            this.Controls.Add(this.startDateTimePicker1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker1;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ MyCourseWork/Form1.cs
@@ -0,0 +1,193 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCourseWork
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+        private double monthlyEarnings;
+        private string selectedEmployeeName = string.Empty;
+
+        public Form1()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+            LoadEmployeeList();
+            SetTextBoxesReadOnly(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Виберіть працівника із списку";
+            label1.ForeColor = Color.Blue;
+            label2.Text = "Посада:";
+            label3.Text = "Середньоденна зарплата:";
+            label4.Text = "Заробітна плата:";
+            label5.Text = "Відпустка з";
+            label6.Text = "по";
+            label7.Text = "Кількість відпускних днів:";
+            label8.Text = "Нараховано:";
+            label8.ForeColor = Color.Blue;
+            label9.Text = "Оберіть дати";
+            label9.ForeColor = Color.Blue;
+            label10.Text = "Інформація про працівника";
+            label10.ForeColor = Color.Blue;
+            button1.Text = "Нарахувати відпускні";
+            button1.BackColor = Color.RoyalBlue;
+            button1.ForeColor = Color.White;
+        }
+
+        private void SetTextBoxesReadOnly(bool readOnly)
+        {
+            textBox1.ReadOnly = readOnly;
+            textBox2.ReadOnly = readOnly;
+            textBox3.ReadOnly = readOnly;
+            textBox4.ReadOnly = readOnly;
+            textBox5.ReadOnly = readOnly;
+        }
+
+        private void LoadEmployeeList()
+        {
+            string filePath = @"C:\Users\victo\OneDrive\Документы\my\2-ий курс\OOP\MyCourseWork\MyCourseWork\bin\Debug\personal.xlsx";
+
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    table = result.Tables[0];
+                }
+            }
+
+            listBox1.DataSource = table;
+            listBox1.ValueMember = table.Columns[0].ColumnName;
+            listBox1.DisplayMember = table.Columns[0].ColumnName;
+        }
+
+        private int getDaysOffNumber(DateTime startDate, DateTime endDate)
+        {
+            int daysOff = 0;
+            for (DateTime date = startDate; date < endDate; date = date.AddDays(1))
+            {
+                daysOff++;
+            }
+            return daysOff;
+        }
+
+        private void UpdateDaysOffNumber()
+        {
+            if (startDateTimePicker1.Value > endDateTimePicker2.Value)
+            {
+                MessageBox.Show("Дата початку не може бути пізнішою, ніж дата кінця, або співпадати з нею.");
+                textBox3.Text = string.Empty;
+                return;
+            }
+
+            int daysOffNumber = getDaysOffNumber(startDateTimePicker1.Value, endDateTimePicker2.Value);
+
+            if (daysOffNumber > 24)
+            {
+                MessageBox.Show("Кількість відпусткових днів не може перевищувати 24.");
+                textBox3.Text = string.Empty;
+                return;
+            }
+
+            textBox3.Text = daysOffNumber.ToString();
+
+        }
+
+        private double CalculateTotalSalary(int daysOffNumber)
+        {
+            double averageEarningForDay = CalculateAverageEarningForDay();
+
+            double totalSalary = averageEarningForDay * daysOffNumber;
+
+            return totalSalary;
+        }
+
+        private double CalculateAverageEarningForDay()
+        {
+            double maxAverageEarningForDay = 3301.58;
+
+            double averageEarningForDay = monthlyEarnings * 12 / 365;
+
+            if (averageEarningForDay > maxAverageEarningForDay)
+            {
+                averageEarningForDay = maxAverageEarningForDay;
+            }
+
+            return Math.Round(averageEarningForDay);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                DataRowView selectedRow = (DataRowView)listBox1.SelectedItem;
+
+                string fullName = selectedRow.Row[0].ToString();
+
+                DataRow[] filteredRows = table.Select($"{table.Columns[0].ColumnName} = '{fullName}'");
+
+
+                if (filteredRows.Length > 0)
+                {
+                    selectedEmployeeName = fullName;
+                    monthlyEarnings = double.Parse(filteredRows[0][2].ToString());
+
+                    textBox1.Text = filteredRows[0][1].ToString();
+                    textBox2.Text = filteredRows[0][2].ToString();
+                    textBox5.Text = CalculateAverageEarningForDay().ToString();
+
+                    textBox4.Text = string.Empty;
+                    startDateTimePicker1.Value = DateTime.Today;
+                    endDateTimePicker2.Value = DateTime.Today;
+                    textBox3.Text = string.Empty;
+                }
+            }
+        }
+
+        private void startDateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = string.Empty;
+            UpdateDaysOffNumber();
+        }
+
+        private void endDateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = string.Empty;
+            UpdateDaysOffNumber();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int daysOffNumber = getDaysOffNumber(startDateTimePicker1.Value, endDateTimePicker2.Value);
+
+            if (!string.IsNullOrEmpty(selectedEmployeeName))
+            {
+                double totalSalary = CalculateTotalSalary(daysOffNumber);
+
+                if (daysOffNumber > 0 && textBox3.Text != string.Empty)
+                {
+                    textBox4.Text = totalSalary.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Оберіть коректно дати початку та кінця відпустки");
+                }
+            }
+        }
+    }
+}
